6c12fa5940673ba767a327cb81ad27ee22dba369
Estevan Bellino <bellino.estevan@gmail.com>
2018-03-27 22:26:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,3 @@ function shout(string){
   return string.toUpperCase()
 }
 
-function whisper(string){
-  return string.toLowerCase
-}
-if(string===shout){
-  return string.toUpperCase()
-}
-if(string===whisper){
-  return string.toLowerCase()
-}
