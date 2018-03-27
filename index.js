cfb402276509be70c538703167281eada5025648
Estevan Bellino <bellino.estevan@gmail.com>
2018-03-27 21:58:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 function shout(string){
   return string.toUpperCase()
 }
-function whisper(string){
-  return string.toLowerCase()
-}
-funtion; shout(string){
-  console.log("HELLO!");
-}
-function sayHiToGrandma(string){
-  return
-}
